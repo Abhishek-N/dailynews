403a4fa877a083f9a6c16fed23a562b83f8a44cf
Abhishek-N <abhisheknshetty97@gmail.com>
2022-03-13 19:25:15 +0530

    add registration

--- news/views.py
+++ news/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import redirect, render
 from django.contrib.auth import authenticate
+from django.contrib.auth.models import User
+from ipware import get_client_ip
+from ip2geotools.databases.noncommercial import DbIpCity
 
 # Create your views here.
 
@@ -39,3 +42,50 @@ def register(request):
         if password1 != password2:
             msg = "Your Passwords didn't match"
             return render(request, 'front/msgbox.html', {'msg': msg})
+
+        check1 = 0
+        check2 = 0
+        check3 = 0
+        check4 = 0
+
+
+        for i in password1:
+            
+            if i > 0 and i < 9:
+                check1 = 1
+
+            if i > 'A' and i < 'Z':
+                check2 = 1
+
+            if i > 'a' and i < 'z':
+                check3 = 1
+
+            if i > '!' and i < '(':
+                check4 = 1
+
+        if check1 == 0 or check2 == 0 or check3 == 0 or check4 == 0:
+            msg = "Your password is not strong enough"
+            return render(request, 'front/msgbox.html', {'msg':msg})
+
+        if len(password1) < 8:
+            msg = "Your password must be atleast 8 charecters long"
+            return render(request, 'front/msgbox.html', {'msg':msg})
+
+        if len(User.objects.filter(username=uname)) == 0 and len(User.objects.filter(email=email)) == 0:
+
+            ip, is_routable = get_client_ip(request)
+
+            if ip is None:
+                ip = "0.0.0.0"
+
+            try: 
+                response = DbIpCity.get(ip, api_key='free')
+                country = response.country + " | " + response.city
+
+            except:
+                country = "Unknown"
+
+            user = User.objects.create_user(username=uname, email=email, password=password1)
+            # b = Manager(name=name, utxt=uname, email=email, ip=ip, country=country)
+            # b.save()
+    return render(request, 'front/login.html')
